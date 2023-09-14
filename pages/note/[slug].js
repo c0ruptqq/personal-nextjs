28893de924041f4d94335d0840945e063e91ec87
Maksym <max@Maksyms-MacBook-Air.local>
2023-09-14 20:11:48 +0100

    Purged unecessary files in katex

--- pages/note/[slug].js
+++ pages/note/[slug].js
@@ -2,17 +2,8 @@ import fs from 'fs'
 import matter from 'gray-matter'
 import MarkdownIt from 'markdown-it'
 import mk from '../katex/katex'
-import blockUml from '../katex/blockPlantuml'
-import codeUml from '../katex/plantuml'
-import image from '../katex/image'
-import emoji from 'markdown-it-emoji'
-import taskLists from 'markdown-it-task-lists'
-import footnote from 'markdown-it-footnote'
-import markdownDeflist from 'markdown-it-deflist'
-import markdownItToc from 'markdown-it-toc-done-right'
-
 import { globSync } from 'glob'
-const anchorSymbol = '<svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>'
+
 
 
 
@@ -54,39 +45,14 @@ const DEFAULT_OPTIONS = {
 
 const mkit = {}
 const katex = {}
-const uml = {}
-const hideYamlMeta = 1
-const sequenceDiagrams = {}
-const flowchartDiagrams = {}
-const toc = {}
 var md = new MarkdownIt({
   ...DEFAULT_OPTIONS.mkit,
   ...mkit
 })
-// if (hideYamlMeta === 1) {
-//   md.use(meta([['---', '\\.\\.\\.'], ['---', '\\.\\.\\.']]))
-// }
 md
   .use(mk, {
     ...DEFAULT_OPTIONS.katex,
     ...katex
-  })
-  .use(blockUml, {
-    ...DEFAULT_OPTIONS.uml,
-    ...uml
-  })
-  .use(codeUml, {
-    ...DEFAULT_OPTIONS.uml,
-    ...uml
-  })
-  .use(emoji)
-  .use(taskLists)
-  .use(markdownDeflist)
-  .use(footnote)
-  .use(image)
-  .use(markdownItToc, {
-    ...DEFAULT_OPTIONS.toc,
-    ...toc
   })
 
 export async function getStaticPaths() {
@@ -102,21 +68,6 @@ export async function getStaticPaths() {
     fallback: true
   }
 }
-
-//export async function getStaticPaths() {
-//  const files = fs.readdirSync('posts')
-//  const paths = files.map((fileName) => ({
-//    params: {
-//      slug: fileName.replace('.md', '')
-//    }
-//  }))
-//  console.log(paths)
-//  return {
-//    paths,
-//    fallback: false
-//  }
-//}
-
 export async function getStaticProps({ params: { slug } }) {
   const fileName = fs.readFileSync(`posts/notes/${slug}.md`, 'utf8')
   const { data: frontmatter, content } = matter(fileName)
